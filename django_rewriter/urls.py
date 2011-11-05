7fbda5db9caf7d5a8d91015bf4faf994b67a1766
MoR <mor@mor-HP-ProBook-4720s.(none)>
2011-11-05 19:08:02 +0300

    I Create registration, but not exist logining

--- django_rewriter/urls.py
+++ django_rewriter/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^accounts/', include('registration.backends.simple.urls')),
     # Examples:
     # url(r'^$', 'django_rewriter.views.home', name='home'),
     # url(r'^django_rewriter/', include('django_rewriter.foo.urls')),
@@ -13,5 +14,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
