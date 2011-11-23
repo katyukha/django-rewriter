e881af93433974b51d244f9e017a8d3a4546ac01
Dmytro Katyukha <firemage.dima@gmail.com>
2011-11-24 00:36:12 +0200

    few bugfixes.

--- django_rewriter/product/form.py
+++ django_rewriter/product/form.py
@@ -3,10 +3,10 @@ from django_rewriter.product.models import Product
 
 class ProductForm(ModelForm):
 
-    def __init__(self, user, *args, **kwargs):
+    def __init__(self, user_profile, *args, **kwargs):
         super(ProductForm, self).__init__(*args, **kwargs)
         instance = getattr(self, 'instance', None)
-        if instance and instance.id and not user.is_seo:
+        if instance and instance.id and not user_profile.is_seo:
             self.fields['meta_title'].widget.attrs['readonly'] = True
             self.fields['meta_keywords'].widget.attrs['readonly'] = True
             self.fields['meta_desc'].widget.attrs['readonly'] = True
